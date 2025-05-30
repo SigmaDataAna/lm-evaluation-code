dcb75f443933f5fb20ff41599588ea43e4f621d0
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-05-30 11:43:23 +0000

    update

--- test_scripts/test_ms.sh
+++ test_scripts/test_ms.sh
@@ -3,7 +3,7 @@ export HF_ALLOW_CODE_EVAL=1
 
 lm_eval --model hf \
     --model_args pretrained=Qwen/Qwen2.5-Coder-1.5B-Instruct \
-    --task humaneval \
+    --task HumanEval-MultiLineInfillingLight \
     --batch_size 64 \
     --confirm_run_unsafe_code \
     --trust_remote_code

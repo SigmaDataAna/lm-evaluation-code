dccf872dc8a27b250a5c43e6cf3c07705d922fe9
anonymous <2001112210@pku.edu.cn>
2025-05-30 08:18:51 +0000

    update

--- test_scripts/test_llama.sh
+++ test_scripts/test_llama.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES="2,3"
 
 lm_eval --model hf \
     --model_args pretrained=/mnt/hdd1/huggingfacecache/hf_cache_lizongyang/models--Qwen--Qwen3-0.6B/snapshots/6130ef31402718485ca4d80a6234f70d9a4cf362 \
-    --tasks humaneval \
+    --tasks HumanEval-MultiLineInfillingLight \
     --batch_size 8 \
     --confirm_run_unsafe_code \
     --trust_remote_code \

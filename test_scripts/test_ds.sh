efeb382531acade54a799d30de31f24184e0ac97
Shuai Lu <shuailu@microsoft.com>
2025-05-31 13:52:15 +0000

    update

--- test_scripts/test_ds.sh
+++ test_scripts/test_ds.sh
@@ -1,11 +1,38 @@
 
 export HF_ALLOW_CODE_EVAL=1
 
-accelerate launch -m lm_eval --model hf \
-    --model_args pretrained=../hf_iter_10000/ \
-    --tasks humaneval_instruct_sigma \
-    --batch_size 32 \
+accelerate launch -m lm_eval \
+    --model hf \
+    --model_args pretrained=deepseek-ai/DeepSeek-Coder-V2-Lite-Base,dtype="bfloat16",parallelize=True \
+    --task HumanEval-SingleLineInfilling \
+    --batch_size 256 \
     --confirm_run_unsafe_code \
     --trust_remote_code \
-    --log_samples \
-    --output_path ../outputs
+    2>&1 | tee "${SAVE_PATH}/coderv2lite_eval_mscode_singleline.txt"
+
+accelerate launch -m lm_eval \
+    --model hf \
+    --model_args pretrained=deepseek-ai/DeepSeek-Coder-V2-Lite-Base,dtype="bfloat16",parallelize=True \
+    --task HumanEval-MultiLineInfillingLight \
+    --batch_size 256 \
+    --confirm_run_unsafe_code \
+    --trust_remote_code \
+    2>&1 | tee "${SAVE_PATH}/coderv2lite_eval_mscode_multiline.txt"
+
+accelerate launch -m lm_eval \
+    --model hf \
+    --model_args pretrained=deepseek-ai/DeepSeek-V2-Lite,dtype="bfloat16",parallelize=True \
+    --task HumanEval-SingleLineInfilling \
+    --batch_size 256 \
+    --confirm_run_unsafe_code \
+    --trust_remote_code \
+    2>&1 | tee "${SAVE_PATH}/v2lite_eval_mscode_singleline.txt"
+
+accelerate launch -m lm_eval \
+    --model hf \
+    --model_args pretrained=deepseek-ai/DeepSeek-V2-Litedtype="bfloat16",parallelize=True \
+    --task HumanEval-MultiLineInfillingLight \
+    --batch_size 256 \
+    --confirm_run_unsafe_code \
+    --trust_remote_code \
+    2>&1 | tee "${SAVE_PATH}/v2lite_eval_mscode_multiline.txt"

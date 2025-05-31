a1decaa2c41ea6beb6e2267b5d79b26816a56071
Shuai Lu <shuailu@microsoft.com>
2025-05-31 04:09:07 +0000

    update

---
+++ test_scripts/test_sigma.sh
@@ -0,0 +1,12 @@
+
+export HF_ALLOW_CODE_EVAL=1
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 lm_eval \
+    --model hf \
+    --model_args pretrained="${HUGGINGFACE_CKPT_PATH}",dtype="bfloat16",parallelize=True \
+    --task HumanEval-MultiLineInfillingLight \
+    --batch_size 64 \
+    --confirm_run_unsafe_code \
+    --trust_remote_code \
+    -o "${HUGGINGFACE_CKPT_PATH}/eval_mscode_multiline.json" \
+    2>&1 | tee "${HUGGINGFACE_CKPT_PATH}/eval_mscode_multiline.txt"

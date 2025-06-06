c4a1f122b9f0146ed59d5494c84ac0b58d15c127
tianyu <chentianyu@microsoft.com>
2025-06-06 07:02:25 +0000

    update

--- test_scripts/test_sigma_lite_local.sh
+++ test_scripts/test_sigma_lite_local.sh
@@ -1,25 +1,22 @@
 
 export HF_ALLOW_CODE_EVAL=1
-export OUTPUT_DIR="/mnt/blob-shuailu1-tianyu-out/evaluation_results/hf_iter_220000"
 
-azcopy copy --recursive "https://hptrainingwestus.blob.core.windows.net/pretraining/checkpoints/OpenPAI-Pretrain-Lite-Code-FIM-v2/hf_iter_220000/${SAS_KEY}" .
-
-lm_eval \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 lm_eval \
     --model hf \
-    --model_args pretrained="./hf_iter_220000",dtype="bfloat16" \
+    --model_args pretrained="${HUGGINGFACE_CKPT_PATH}",dtype="bfloat16",parallelize=True \
     --task humaneval \
     --batch_size 256 \
     --confirm_run_unsafe_code \
     --trust_remote_code \
-    --log_samples \
-    --output_path ${OUTPUT_DIR}
+    -o "${HUGGINGFACE_CKPT_PATH}/eval/eval_mscode_singleline.json" \
+    2>&1 | tee "${HUGGINGFACE_CKPT_PATH}/eval/eval_mscode_singleline.txt"
 
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 lm_eval \
-#     --model hf \
-#     --model_args pretrained="${HUGGINGFACE_CKPT_PATH}",dtype="bfloat16",parallelize=True \
-#     --task HumanEval-MultiLineInfillingLight \
-#     --batch_size 256 \
-#     --confirm_run_unsafe_code \
-#     --trust_remote_code \
-#     -o "${HUGGINGFACE_CKPT_PATH}/eval_mscode_multiline.json" \
-#     2>&1 | tee "${HUGGINGFACE_CKPT_PATH}/eval_mscode_multiline.txt"
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 lm_eval \
+    --model hf \
+    --model_args pretrained="${HUGGINGFACE_CKPT_PATH}",dtype="bfloat16",parallelize=True \
+    --task mbpp \
+    --batch_size 256 \
+    --confirm_run_unsafe_code \
+    --trust_remote_code \
+    -o "${HUGGINGFACE_CKPT_PATH}/eval/eval_mscode_multiline.json" \
+    2>&1 | tee "${HUGGINGFACE_CKPT_PATH}/eval/eval_mscode_multiline.txt"
